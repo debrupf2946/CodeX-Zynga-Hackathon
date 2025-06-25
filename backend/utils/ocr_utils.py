90c257e8f57d0403f7542dd7c79c0a24514e4bd6
Azmeen Khatoon <azmeenkhatoon2704@gmail.com>
2025-06-25 17:56:52 +0530

    Update ocr_utils.py

--- backend/utils/ocr_utils.py
+++ backend/utils/ocr_utils.py
@@ -4,7 +4,15 @@ import easyocr
 import re
 from datetime import datetime
 import torch
+from pdf2image import convert_from_path
+import os
 
+def convert_pdf_to_image(pdf_path):
+    images = convert_from_path(pdf_path)
+    img_path = pdf_path.replace('.pdf', '.jpg')
+    images[0].save(img_path, 'JPEG')
+    return img_path
+    
 def compute_age(dob_str):
     dob = datetime.strptime(dob_str, "%d/%m/%Y")
     today = datetime.today()
@@ -12,12 +20,13 @@ def compute_age(dob_str):
     return age
 
 def ocr_extract_info(image_path):
+    if image_path.lower().endswith(".pdf"):
+        image_path = convert_pdf_to_image(image_path)
     # Aadhaar languages: en, hi, as, bn, gu, kn, ml, mr, or, pa, ta, te, ur
     aadhaar_langs = [
         'en', 'hi', 'as', 'bn', 'gu', 'kn', 'ml', 'mr',
         'or', 'pa', 'ta', 'te', 'ur'
     ]
-
     # Auto-detect GPU
     gpu_available = torch.cuda.is_available()
     print(f"👉 EasyOCR — using GPU: {gpu_available}")
