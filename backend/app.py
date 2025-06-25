7874e4e215346af261346f3e49fa0eb2b8be7419
Mamta <122298524+Mamtagithubrit@users.noreply.github.com>
2025-06-25 17:24:22 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -3,9 +3,19 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
 from utils.ocr_utils import ocr_extract_info
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 CORS(app)  # Allow requests from frontend
+
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'pdf'}
+app.config['MAX_CONTENT_LENGTH'] = 4 * 1024 * 1024  # 4MB
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 
 UPLOAD_FOLDER = 'static/'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
@@ -15,8 +25,12 @@ def upload_aadhaar():
     if 'aadhaar_image' not in request.files:
         return jsonify({"error": "No file uploaded"}), 400
 
-    file = request.files['aadhaar_image']
-    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+   file = request.files['aadhaar_image']
+    if not allowed_file(file.filename):
+        return jsonify({"error": "Invalid file format"}), 400
+
+    filename = secure_filename(file.filename)
+    file_path = os.path.join(UPLOAD_FOLDER, filename)
     file.save(file_path)
 
     result = ocr_extract_info(file_path)
